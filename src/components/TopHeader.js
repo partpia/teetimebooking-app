97b8379cdb2c2128c209a3f6ec4f3bbff8d1fbfe
partpia <partanenppia@gmail.com>
2022-04-02 23:19:56 +0300

    added links for signup and login

--- src/components/TopHeader.js
+++ src/components/TopHeader.js
@@ -1,16 +1,27 @@
 import React from "react";
-import { PageHeader } from "antd";
+import { Button, PageHeader } from "antd";
+import { Link } from "react-router-dom";
 import MenuRightSide from "./MenuRightSide";
 
 const TopHeader = () => {
 
-    return(
+    return (
         <PageHeader
             className="header"
             title="Riviera Maya del Sol Golf Club"
             subTitle="Sun is always shining and the greens are greener"
             extra={[
-                <MenuRightSide />
+                <Button
+                    key="signin"
+                    type="text">
+                    <Link to="/signup">Sign Up</Link>
+                </Button>,
+                <Button
+                    key="login"
+                    type="text">
+                    <Link to="/login">Log In</Link>
+                </Button>,
+                <MenuRightSide key="menuright" />
             ]}
         />
     );
