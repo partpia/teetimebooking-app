4e7b51fd0f97a9da031234c3d073a658c4e3da6e
partpia <partanenppia@gmail.com>
2022-06-02 16:52:21 +0300

    Added info-Alert

--- src/components/TeeTimeBooking.js
+++ src/components/TeeTimeBooking.js
@@ -1,26 +1,36 @@
 import React from "react";
 import { Content } from "antd/lib/layout/layout";
 import {
-    Row,
+    Alert,
+    Col,
+    Divider,
     Tabs,
     Typography
 } from 'antd';
 import '../App.css';
-import BookingCalendar from "./BookingCalendar";
+import CalendarCourseOne from "./CalendarCourseOne";
 import CalendarCourseTwo from "./CalendarCourseTwo";
 
 const TeeTimeBooking = () => {
-    const { Title } = Typography;
+    const { Title, Paragraph } = Typography;
     const { TabPane } = Tabs;
 
     return (
         <Content className="tee-time-booking-content">
-            <Row>
-                <Title level={3}>Select course ... info </Title>
-            </Row>
+            <Col className="tee-time-booking-info">
+                <Title level={3}>Select course</Title>
+                <Divider className="divider-style"></Divider>
+                <Alert
+                message="HCP limits"
+                description="The maximum Handicap Index is 54 for men and women in both courses. Group's maximum HCP is 110."
+                type="info"
+                showIcon
+            />
+            </Col>
+
             <Tabs type="card">
-                <TabPane tab="Course 18 holes" key="1">
-                    <BookingCalendar />
+                <TabPane tab="Course 18 holes" key="1" className="tee-time-booking-tab">
+                    <CalendarCourseOne />
                 </TabPane>
                 <TabPane tab="Course 9 holes" key="2">
                     <CalendarCourseTwo />
