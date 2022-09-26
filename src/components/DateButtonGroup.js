ca68f1e3049365534f170a2ff637276f47386b5c
partpia <partanenppia@gmail.com>
2022-09-26 10:09:43 +0300

    Added functions for buttons (next and previous day)

--- src/components/DateButtonGroup.js
+++ src/components/DateButtonGroup.js
@@ -4,27 +4,33 @@ import {
     Radio,
     Row
 } from 'antd';
-import '../App.css';
 import { LeftOutlined, RightOutlined } from '@ant-design/icons';
-import { add, format } from 'date-fns';
+import {
+    add,
+    format,
+    isAfter,
+    isBefore,
+    isEqual,
+    parseISO,
+    sub
+} from 'date-fns';
+import '../App.css';
 
-const DateButtonGroup = ( props ) => {
+const DateButtonGroup = (props) => {
     const today = format(new Date(), 'yyyy-MM-dd');
     const [buttonGroup, setButtonGroup] = useState([]);
     const [value, setValue] = useState(today);
 
-    useEffect(() => {
-        handleDates();
-    }, []);
+    useEffect(() => handleDates(), []);
 
     const handleDates = () => {
         let day = new Date();
         let tempDates = [{
-            label: 'today', value: format(day, 'yyyy-MM-dd')
+            label: 'Today', value: format(day, 'yyyy-MM-dd')
         }];
         for (let i = 1; i < 7; i++) {
             let date = add(day, { days: i, })
-            tempDates.push({ label: format(date, 'E d.M'), value: format(date, 'yyyy-MM-dd') })
+            tempDates.push({ label: format(date, 'E d.M.'), value: format(date, 'yyyy-MM-dd') })
         }
         setButtonGroup(tempDates);
     }
@@ -32,13 +38,34 @@ const DateButtonGroup = ( props ) => {
         setValue(e.target.value);
         props.getTeeTimes(e.target.value);
     }
+    // tee time bookings of the previous day
+    const getPreviousDay = () => {
+        let prevDate = sub(parseISO(value), { days: 1 });
+
+        if (isAfter(prevDate, parseISO(today)) || isEqual(prevDate, parseISO(today))) {
+            let formattedPrevDate = format(prevDate, 'yyyy-MM-dd')
+            setValue(formattedPrevDate);
+            props.getTeeTimes(formattedPrevDate);
+        }
+    }
+    // tee time bookings of the next day
+    const getNextDay = () => {
+        let nextDate = add(parseISO(value), { days: 1, });
+        let lastDate = parseISO(buttonGroup[6].value);
+        
+        if (isBefore(nextDate, lastDate) || isEqual(nextDate, lastDate)) {
+            let formattedNextDate = format(nextDate, 'yyyy-MM-dd')
+            setValue(formattedNextDate);
+            props.getTeeTimes(formattedNextDate);
+        }
+    }
 
     return (
         <Row className="booking-calendar-buttons">
             <Button
+                onClick={getPreviousDay}
                 icon={<LeftOutlined />}
-                size="large"
-                style={{ marginRight: 15 }} />
+                size="large" />
             <Radio.Group
                 options={buttonGroup}
                 value={value}
@@ -46,10 +73,10 @@ const DateButtonGroup = ( props ) => {
                 optionType="button"
                 size="large"
             />
-            <Button 
+            <Button
+                onClick={getNextDay}
                 icon={<RightOutlined />}
-                size="large"
-                style={{ marginLeft: 15 }} />
+                size="large" />
         </Row>
     );
 }
