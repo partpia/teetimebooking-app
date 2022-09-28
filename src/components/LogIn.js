0b16025da95ac1e3c7df89390b295cd5c674d23b
partpia <partanenppia@gmail.com>
2022-09-28 10:37:03 +0300

    Added state for user and input fields for username and password

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+    EyeInvisibleOutlined,
+    EyeTwoTone
+} from '@ant-design/icons';
+import { Input, Space } from 'antd';
 
 const LogIn = () => {
+    const [user, setUser] = useState({
+        username: '',
+        pwd: ''
+    });
 
-    //TODO
-    
+    const handleChange = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value })
+    }
+
     return (
-        <div></div>
+        <Space direction="vertical" size="middle" className="log-in">
+            <Input
+                name="username"
+                onChange={handleChange}
+                size="large"
+                placeholder="Username"
+            />
+            <Input.Password
+                name="pwd"
+                onChange={handleChange}
+                size="large"
+                placeholder="Password"
+                iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+            />
+        </Space>
     );
 }
 export default LogIn;
