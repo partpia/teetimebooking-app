3ea35b81e9fbe1ebd8f9c1a98d14588b1ff9a873
partpia <partanenppia@gmail.com>
2022-09-29 11:53:08 +0300

    div changed to Layout and new route for account added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import TopHeader from './components/TopHeader';
 import PageFooter from './components/PageFooter';
 import PageContent from './components/PageContent';
 import ErrorPage from "./components/ErrorPage";
 import SignUp from "./components/SignUp";
 import TeeTimeBooking from "./components/TeeTimeBooking";
+import Profile from "./components/Profile";
 import './App.css';
+import { Layout } from "antd";
 
 function App() {
 
   return (
-    <div className="App">
-        <TopHeader />
-        <Routes>
-          <Route path="/" element={<PageContent />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="teetimes" element={<TeeTimeBooking />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-        <PageFooter />
-    </div>
+    <Layout className="App">
+      <TopHeader />
+      <Routes>
+        <Route path="/" element={<PageContent />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/teetimes" element={<TeeTimeBooking />} />
+        <Route path="/account" element={<Profile />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <PageFooter />
+    </Layout>
   );
 }
-
 export default App;
