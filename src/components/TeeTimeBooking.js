c03e035e0df4bbed26d83a1716b0351e445033ed
partpia <partanenppia@gmail.com>
2022-10-21 08:57:39 +0300

    Changed the size of the tabs to large and font weight to bold

--- src/components/TeeTimeBooking.js
+++ src/components/TeeTimeBooking.js
@@ -12,27 +12,33 @@ import CalendarCourseOne from "./CalendarCourseOne";
 import CalendarCourseTwo from "./CalendarCourseTwo";
 
 const TeeTimeBooking = () => {
-    const { Title, Paragraph } = Typography;
+    const { Title } = Typography;
     const { TabPane } = Tabs;
 
     return (
         <Content className="tee-time-booking-content">
             <Col className="tee-time-booking-info">
-                <Title level={3}>Select course</Title>
-                <Divider className="divider-style"></Divider>
+                <Divider orientation="left">
+                    <Title level={3}>Select course and book a tee time</Title>
+                </Divider>
                 <Alert
-                message="HCP limits"
-                description="The maximum Handicap Index is 54 for men and women in both courses. Group's maximum HCP is 110."
-                type="info"
-                showIcon
-            />
+                    message="HCP limits"
+                    description="The maximum Handicap Index is 54 for men and women in both courses. Group's maximum HCP is 110."
+                    type="info"
+                    showIcon
+                />
             </Col>
-
-            <Tabs type="card">
-                <TabPane tab="Course 18 holes" key="1" className="tee-time-booking-tab">
+            <Tabs
+                type="card"
+                size="large"
+                tabBarStyle={{
+                    fontWeight: "bold",
+                }}
+                tabBarGutter={8}>
+                <TabPane tab="Holy Round 18 holes" key="1" className="tee-time-booking-tab">
                     <CalendarCourseOne />
                 </TabPane>
-                <TabPane tab="Course 9 holes" key="2">
+                <TabPane tab="Half of Joy 9 holes" key="2" className="tee-time-booking-tab">
                     <CalendarCourseTwo />
                 </TabPane>
             </Tabs>
