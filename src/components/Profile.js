480da552d3a4c692aa4df1e50e65e26d9ec46ede
partpia <partanenppia@gmail.com>
2022-10-21 08:53:04 +0300

    New component for profile information

---
+++ src/components/Profile.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Profile = () => {
+
+    // TODO: User's profile information
+
+    return(
+        <div>
+            Profile information coming soon.
+        </div>        
+    )
+}
+export default Profile;
