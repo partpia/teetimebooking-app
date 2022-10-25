0c3edb5c7f1e02d8df729fb2b2450f3b4fc82739
partpia <partanenppia@gmail.com>
2022-10-25 10:55:06 +0300

    Removed title and alert and used paragraphs instead

--- src/components/TeeTimeBooking.js
+++ src/components/TeeTimeBooking.js
@@ -1,39 +1,44 @@
 import React from "react";
 import { Content } from "antd/lib/layout/layout";
 import {
-    Alert,
     Col,
-    Divider,
+    Row,
+    Space,
     Tabs,
     Typography
 } from 'antd';
+import { InfoCircleOutlined } from '@ant-design/icons';
 import '../App.css';
 import CalendarCourseOne from "./CalendarCourseOne";
 import CalendarCourseTwo from "./CalendarCourseTwo";
 
 const TeeTimeBooking = () => {
-    const { Title } = Typography;
+    const { Paragraph } = Typography;
     const { TabPane } = Tabs;
 
     return (
         <Content className="tee-time-booking-content">
-            <Col className="tee-time-booking-info">
-                <Divider orientation="left">
-                    <Title level={3}>Select course and book a tee time</Title>
-                </Divider>
-                <Alert
-                    message="HCP limits"
-                    description="The maximum Handicap Index is 54 for men and women in both courses. Group's maximum HCP is 110."
-                    type="info"
-                    showIcon
-                />
-            </Col>
+            <Row align="top">
+                <Col span={12} className="tee-time-booking-info-col-left">
+                    <Paragraph className="tee-time-booking-info-p">Select course and book a tee time</Paragraph>
+                </Col>
+                <Col span={12} className="tee-time-booking-info-col-right">
+                    <Space direction="vertical" className="tee-time-booking-hcp-info">
+                        <Paragraph >
+                            <InfoCircleOutlined />&nbsp;
+                            HCP limits
+                        </Paragraph>
+                        <Paragraph>
+                            The maximum Handicap Index is 54 for men and women in both courses. Group's maximum HCP is 110.
+                        </Paragraph>
+
+                    </Space>
+                </Col>
+            </Row>
             <Tabs
                 type="card"
                 size="large"
-                tabBarStyle={{
-                    fontWeight: "bold",
-                }}
+                className="test"
                 tabBarGutter={8}>
                 <TabPane tab="Holy Round 18 holes" key="1" className="tee-time-booking-tab">
                     <CalendarCourseOne />
@@ -42,7 +47,7 @@ const TeeTimeBooking = () => {
                     <CalendarCourseTwo />
                 </TabPane>
             </Tabs>
-        </Content>
+        </Content >
     );
 }
 export default TeeTimeBooking;
