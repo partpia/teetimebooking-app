74bfbbed2f51873fd56ff47a32fa2ae96cd9992b
partpia <partanenppia@gmail.com>
2022-10-25 10:29:30 +0300

    After logging out navigates to main page

--- src/components/LogOut.js
+++ src/components/LogOut.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Button } from "antd";
+import { useNavigate } from "react-router-dom";
 import '../App.css';
 
 const LogOut = (props) => {
     const buttonStyle = { fontSize: '28px' }
+    let navigate = useNavigate();
 
     const logOut = () => {
         sessionStorage.removeItem("jwt");
         props.setIsAuth(false);
         props.closeDrawer();
+        navigate("/");
     }
 
     return (
